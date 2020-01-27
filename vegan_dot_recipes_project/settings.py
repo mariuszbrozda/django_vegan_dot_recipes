5a9ec539110d63b915f6d7698149236f69f70e7d
Ubuntu <ubuntu@ip-172-31-94-95.ec2.internal>
2020-01-27 23:16:57 +0000

    add https://vegan-dot-recipes-django.herokuapp.com/ to allowed hosts

--- vegan_dot_recipes_project/settings.py
+++ vegan_dot_recipes_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "SECRET_KEY"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), '127.0.0.1', 'f995c0db3cd24e759471ec9888ef75ff.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), '127.0.0.1','https://vegan-dot-recipes-django.herokuapp.com/' 'f995c0db3cd24e759471ec9888ef75ff.vfs.cloud9.us-east-1.amazonaws.com']
 
 if os.environ.get('DEVELOPMENT'):
     development = True
